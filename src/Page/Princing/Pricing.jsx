787e4369c109a575b882b56f79e8250e36aec490
Romain-Delchie <roomsrio7782@gmail.com>
2026-08-26 21:15:17 +0200

    debug pricing css

--- src/Page/Princing/Pricing.jsx
+++ src/Page/Princing/Pricing.jsx
@@ -3,22 +3,21 @@ import "./Pricing.css";
 export default function Pricing() {
   return (
     <section className="pricing" id="pricing">
-
-      <h2>Tarifs</h2>
+      <h2>Tarifs et planning</h2>
 
       <div className="pricing-top">
-
         <div className="pricing-image">
-          <img src="/dos.jpeg" alt="Yoga" />
+          <img src="/planning.jpeg" alt="Yoga" />
         </div>
 
         <div className="pricing-content">
-
           <h3>Yoga Vinyasa - Chaise - Yin Yoga</h3>
 
           <div className="pricing-block">
-
-            <p><strong>Cours collectifs</strong> - petit groupe - 8 personnes maximum</p>
+            <p>
+              <strong>Cours collectifs</strong> - petit groupe - 8 personnes
+              maximum
+            </p>
 
             <p>Durée : 60 mins</p>
 
@@ -34,12 +33,12 @@ export default function Pricing() {
               <li>1 séance par semaine 490€ (13€ la séance)</li>
               <li>2 séances par semaine 760€ (10€ la séance)</li>
             </ul>
-
           </div>
 
           <div className="pricing-block">
-
-            <p><strong>Cours particuliers</strong> : Vinyasa - Chaise & Yin Yoga</p>
+            <p>
+              <strong>Cours particuliers</strong> : Vinyasa - Chaise & Yin Yoga
+            </p>
 
             <p>Durée : 60 mins - en ligne ou en présentiel</p>
 
@@ -50,41 +49,38 @@ export default function Pricing() {
             <p>Tarif modifiable selon la distance.</p>
 
             <p>(possibilité de forfait sur demande)</p>
-
           </div>
-
         </div>
-
       </div>
+      <h2>Yoga en entreprise</h2>
+      <div className="pricing-bottom">
+        <div className="pro-section">
+          <div className="company">
+            <p>
+              Je propose des séances de yoga sur chaise, sur mesure, en fonction
+              de vos besoins.
+            </p>
 
-      <div className="company">
+            <p>
+              Une pause bien-être pour vos collaborateurs, une source de
+              détente, de concentration et de motivation.
+            </p>
 
-        <h3>TARIFS Yoga en entreprise</h3>
+            <p>
+              Chaque entreprise est unique : je vous envoie un devis
+              personnalisé sur demande.
+            </p>
 
-        <p>
-          Je propose des séances de yoga sur chaise, sur mesure,
-          en fonction de vos besoins.
-        </p>
-
-        <p>
-          Une pause bien-être pour vos collaborateurs,
-          une source de détente,
-          de concentration
-          et de motivation.
-        </p>
-
-        <p>
-          Chaque entreprise est unique :
-          je vous envoie un devis personnalisé sur demande.
-        </p>
-
-        <p>
-          Contactez-moi pour en discuter
-          et créer ensemble une offre adaptée.
-        </p>
-
+            <p>
+              Contactez-moi pour en discuter et créer ensemble une offre
+              adaptée.
+            </p>
+          </div>
+          <div className="pro-image">
+            <img src="/entreprise.png" alt="Yoga en entreprise" />
+          </div>
+        </div>
       </div>
-
     </section>
   );
 }
